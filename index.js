0fd6b55a87679a8853a6d856602ccb5a5bb29571
favasprintocards-a11y <favasprintocards@gmail.com>
2026-02-14 16:44:12 +0530

    Remove deprecated MongoDB options

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (!fs.existsSync(uploadDir)) {
 
 // MongoDB Connection (Optional: Only if URI provided)
 if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGODB_URI)
         .then(() => console.log('MongoDB Connected'))
         .catch(err => console.log('MongoDB Connection Error:', err));
 }
